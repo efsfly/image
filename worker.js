9113b8ee7e0f8b92d9c7eae34489b428eedfd630
efsfly <info@earnforsure.com>
2025-03-19 15:17:29 +0530

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,8 +1,10 @@
 // worker.js
-self.addEventListener('message', async (e) => {
-    const { imgData, quality, orientation, index } = e.data;
-    const blob = await compressImage(imgData, quality, orientation);
-    self.postMessage({ result: blob, index });
+self.addEventListener('message', ({ data }) => {
+    const { imgData, quality, orientation } = data;
+    const blobPromise = compressImage(imgData, quality, orientation);
+    blobPromise.then(blob => {
+        self.postMessage({ blob });
+    });
 });
 
 async function compressImage(imgData, quality, orientation) {
@@ -12,9 +14,22 @@ async function compressImage(imgData, quality, orientation) {
 
     // Apply orientation
     switch(orientation) {
-        case 3: ctx.rotate(Math.PI); break;
-        case 6: ctx.rotate(Math.PI/2); break;
-        case 8: ctx.rotate(-Math.PI/2); break;
+        case 3:
+            ctx.translate(img.width, img.height);
+            ctx.rotate(Math.PI);
+            break;
+        case 6:
+            canvas.width = img.height;
+            canvas.height = img.width;
+            ctx.rotate(0.5 * Math.PI);
+            ctx.translate(0, -img.height);
+            break;
+        case 8:
+            canvas.width = img.height;
+            canvas.height = img.width;
+            ctx.rotate(-0.5 * Math.PI);
+            ctx.translate(-img.width, 0);
+            break;
     }
 
     ctx.drawImage(img, 0, 0);
